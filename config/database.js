3e5339895480c566828f3b6e5f4a3dba8707549e
Daniel Stallworth <danielstallworthmit@gmail.com>
2014-08-18 23:40:46 -0500

    installed packages, set up application, connected to database, created routes, and created views.

---
+++ config/database.js
@@ -0,0 +1,8 @@
+// config/database.js
+module.exports = {
+
+	'url' : 'mongodb://user1:password@proximus.modulusmongo.net:27017/Wy3xazup'
+
+	//'your-settings-here' // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
+
+};
