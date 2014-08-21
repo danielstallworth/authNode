4a29aa99417ea20f035ea927bde6c59b8957fb5b
Daniel Stallworth <danielstallworthmit@gmail.com>
2014-08-20 22:35:06 -0500

    Auth change

--- config/auth.js
+++ config/auth.js
@@ -4,18 +4,21 @@
 module.exports = {
 
 	'facebookAuth' : {
-		'clientID' 		: '1467067206897198', //'your-secret-clientID-here', // your App ID
-		'clientSecret' 	: 'ae2e15cba8f2f7f8a9dd479193a9cac0', //'your-client-secret-here', // your App Secret
+		// https://developers.facebook.com/
+		'clientID' 		: 'your-secret-clientID-here', // your App ID
+		'clientSecret' 	: 'your-client-secret-here', // your App Secret
 		'callbackURL' 	: 'http://localhost:8080/auth/facebook/callback'
 	},
 
 	'twitterAuth' : {
-		'consumerKey' 		: 'QVkkvW8ZUaPzO8EXF3FQIW08j', //your-consumer-key-here',
-		'consumerSecret' 	: '4H548Z8YAillP1IAiDYMZuGI8N6N5S1qnEuMBXSiUD3tJRe5kQ', //your-client-secret-here',
+		// https://apps.twitter.com/
+		'consumerKey' 		: 'your-consumer-key-here',
+		'consumerSecret' 	: 'your-client-secret-here',
 		'callbackURL' 		: 'http://localhost:8080/auth/twitter/callback'
 	},
 
 	'googleAuth' : {
+		// https://cloud.google.com/
 		'clientID' 		: 'your-secret-clientID-here',
 		'clientSecret' 	: 'your-client-secret-here',
 		'callbackURL' 	: 'http://localhost:8080/auth/google/callback'
